a6bdbce6d0667ab6419e00f00ae9c3470bd0ca31
Stephen Smith <mail@sasmith.ca>
2026-04-11 17:21:02 -0300

    basic dice collection (#6)

---
+++ src/diceAPI/diceCollection.ts
@@ -0,0 +1,30 @@
+import { Dice } from "./dice.ts";
+import type { DiceResult } from "./types.ts";
+
+export class diceCollection {
+  readonly dice: Dice[];
+  result: DiceResult | undefined = undefined;
+  
+  constructor(dice: Dice[]) {
+    this.dice = dice;
+  }
+
+  roll(): DiceResult {
+    const results = this.dice.map(e => e.roll());
+    const included = results; // not implemented
+    const excluded: number[] = []; // not implemented;
+    const total = results.reduce((a, b) => a + b, 0);
+    this.result = {
+      results,
+      included,
+      excluded,
+      total
+    }
+    return this.result;
+  }
+
+  toString(): string {
+    if(!this.result) return '?';
+    return JSON.stringify(this.result);
+  }
+}
--- test/dice/dice.unit.ts
+++ test/dice/dice.unit.ts
@@ -11,7 +11,7 @@ describe('single dice', () => {
   })
 
   it('should roll within bounds, 1 index', () => {
-    const dice_size = Math.floor(Math.random() * 100);
+    const dice_size = Math.floor(Math.random() * 100) + 1;
     for(let i = 0; i < 1000; i++) {
       const d = new Dice(dice_size);
       assert(d.roll() <= dice_size, `dice rolled invalid ${d.roll()} for size ${dice_size}`);
@@ -20,11 +20,30 @@ describe('single dice', () => {
   })
 
   it('should roll within bounds, 0 index', () => {
-    const dice_size = Math.floor(Math.random() * 100);
+    const dice_size = Math.floor(Math.random() * 100) + 1;
     for(let i = 0; i < 1000; i++) {
       const d = new Dice(dice_size, true);
       assert(d.roll() < dice_size, `dice rolled invalid ${d.roll()} for size ${dice_size}`);
       assert(d.roll() >= 0, `dice rolled invalid ${d.roll()} for size ${dice_size}`);
     }
+  })
+
+  it('should return nothing if not rolled', () => {
+    const d = new Dice(20);
+    assert(d.toString() === '?');
+  })
+
+  it('should return a number when rolled', () => {
+    const d = new Dice(1);
+    d.roll();
+    assert(d.toString() === '1')
+  })
+
+  it('cannot create dice with less than one face', () => {
+    assert.throws(() => new Dice(0));
+  })
+
+  it('cannot create a dice with a non-integer face', () => {
+    assert.throws(() => new Dice(5.5))
   })
 })
