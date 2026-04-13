9a3d9bc941b2dfa954505f98e5e82ce36fb5deff
Stephen Smith <mail@sasmith.ca>
2026-04-13 09:51:42 -0300

    extract routes to a router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { parseDice } from './diceAPI/api.ts';
+import { diceRouter } from './diceAPI/dice.router.ts'
 
 export const app = express()
 const port = 3000
@@ -10,17 +10,7 @@ app.get('/', (_, res) => {
   res.send('Hello, World!')
 })
 
-app.post('/dice', (req, res) => {
-  const body = req.body;
-  try {
-    const dc = parseDice(body['diceString'])
-    const resp = dc.roll();
-    res.send(resp);
-  } catch (e) {
-    res.status(500);
-    res.send(`Failed to parse: diceString: ${body['diceString']}`);
-  }
-})
+app.use(diceRouter);
 
 if (process.argv[1] === new URL(import.meta.url).pathname) {
   app.listen(port, () => {
